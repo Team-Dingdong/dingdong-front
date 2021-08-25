b23fd2f12c18e68c09f236dd5838a70de62a03ad
thdefn <thdefn@gmail.com>
2021-08-25 15:57:32 +0900

    feat : 스와이프 시 채팅방 나가기 다이얼로그 띄우는 기능 구현

--- app/src/main/java/org/techtown/dingdong/chatting/ChattingFragment.java
+++ app/src/main/java/org/techtown/dingdong/chatting/ChattingFragment.java
@@ -1,16 +1,28 @@
 package org.techtown.dingdong.chatting;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import org.jetbrains.annotations.NotNull;
 import org.techtown.dingdong.R;
 
 import java.util.ArrayList;
@@ -40,6 +52,42 @@ public class ChattingFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         chatRoomListAdapter = new ChatRoomListAdapter(getActivity(), chatRooms);
         recyclerView.setAdapter(chatRoomListAdapter);
+
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new SwipeToDeleteCallback(getActivity()) {
+            @Override
+            public void onSwiped(@NonNull @NotNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+                final int position = viewHolder.getAdapterPosition();
+                final ChatRoom chatRoom = chatRoomListAdapter.getChatRooms().get(position);
+
+                //chatRoomListAdapter.removeItem(position);
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+
+                dialog.setMessage("채팅방에서 정말 나가시겠어요? \n무통보 퇴장은 신고 사유가 될 수 있습니다.")
+                        .setTitle(chatRoom.getTitle())
+                        .setPositiveButton("아니오", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.i("Dialog", "아니오");
+                            }
+                        })
+                        .setNegativeButton("네", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.i("Dialog", "네");
+                                chatRoomListAdapter.removeItem(position);
+
+                            }
+                        }).show();
+
+                chatRoomListAdapter.notifyItemChanged(viewHolder.getAdapterPosition());
+            }
+
+        });
+
+        itemTouchHelper.attachToRecyclerView(recyclerView);
 
         return v;
     }
