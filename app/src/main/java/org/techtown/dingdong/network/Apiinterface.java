62ac07d30f5f10caa10369f30c635ea9b70f834d
thdefn <80521474+thdefn@users.noreply.github.com>
2021-09-06 18:46:16 +0900

    fix: 깃 충돌 수정

--- app/src/main/java/org/techtown/dingdong/network/Apiinterface.java
+++ app/src/main/java/org/techtown/dingdong/network/Apiinterface.java
@@ -22,6 +22,7 @@ import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.PATCH;
