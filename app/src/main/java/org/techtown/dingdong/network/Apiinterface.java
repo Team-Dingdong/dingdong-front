a25f4a0268cfa54a977f3102624d9bdb46a4d86b
kimjeonghui <59547003+kimjeonghui@users.noreply.github.com>
2021-09-06 11:39:33 +0900

    [clint][fix]회원가입
    - 회원가입 이미지 , 닉네임 등록

--- app/src/main/java/org/techtown/dingdong/network/Apiinterface.java
+++ app/src/main/java/org/techtown/dingdong/network/Apiinterface.java
@@ -1,18 +1,24 @@
 package org.techtown.dingdong.network;
 
 import org.techtown.dingdong.home.PostResponse;
+import org.techtown.dingdong.login_register.AuthNickRequset;
+import org.techtown.dingdong.login_register.AuthNickResponse;
 import org.techtown.dingdong.login_register.AuthRequest;
 import org.techtown.dingdong.login_register.AuthResponse;
 import org.techtown.dingdong.login_register.LoginRequest;
 import org.techtown.dingdong.login_register.LoginResponse;
+import org.techtown.dingdong.login_register.ProfileUploadResponse;
 import org.techtown.dingdong.profile.MyLatingResponse;
 import org.techtown.dingdong.profile.ProfileResponse;
 
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Query;
 
 public interface Apiinterface {
@@ -41,5 +47,12 @@ public interface Apiinterface {
 
    // @PATCH("/api/v1/upload/profile/:id")
     //Call<ProfileImgResponse>
+
+    //@Multipart
+   // @PATCH("/api/v1/upload/profile/:id")
+    //Call<ProfileUploadResponse> ProfileUploadRequest(@Part MultipartBody.Part file);
+
+    @POST("/api/v1/auth/nickname")
+    Call<AuthNickResponse> AuthNickRequest(@Body AuthNickRequset authNickRequset);
 
 }
